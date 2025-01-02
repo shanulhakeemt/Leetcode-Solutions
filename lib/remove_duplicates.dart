c152dff330b3f13de203ab4b86cbe62ec82fe1f0
shanul hakeem <shanulhakeem60@gmail.com>
2025-01-02 17:50:30 +0530

    26--removeDuplicates solved

---
+++ lib/remove_duplicates.dart
@@ -0,0 +1,24 @@
+/* 
+ ^26. 
+ !Remove Duplicates from Sorted Array
+ */
+
+int removeDuplicates(List<int> nums) {
+  Map<int, int> m = {};
+  int c = 0, i = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (m[nums[i]] != null) {
+      m[nums[i]] = m[nums[i]]! + 1;
+    } else {
+      c++;
+      m[nums[i]] = 1;
+    }
+  }
+  for (int j in m.keys) {
+    nums[i] = j;
+    i++;
+  }
+  return c;
+}
+
+//~ 8 ms Beats 31.41%
---
+++ main.dart
@@ -0,0 +1,6 @@
+import 'package:leetcode_solutions/remove_duplicates.dart';
+
+void main(List<String> arguments) {
+  final result = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+  print(result);
+}
