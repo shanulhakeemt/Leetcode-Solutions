f72479f6817f43d60a426dc79afd325aa2c0e2df
shanul hakeem <shanulhakeem62@gmail.com>
2025-01-09 22:36:20 +0530

    .

--- lib/count_complete_day_pairs_3184.dart
+++ lib/count_complete_day_pairs_3184.dart
@@ -1,3 +1,10 @@
+/* 
+ ^3184. 
+ !count pairs that form a complete day1
+ *Given an integer array hours representing times in hours, return an integer denoting the number of pairs i, j where i < j and hours[i] + hours[j] forms a complete day.
+*A complete day is defined as a time duration that is an exact multiple of 24 hours
+ */
+
 int countCompleteDayPairs(List<int> hours) {
   int c = 0;
   for (int i = 0; i < hours.length; i++) {
