d008dd8ebf43e622b80ef189975b0496a1b25769
shanul hakeem <shanulhakeem62@gmail.com>
2025-01-10 22:41:02 +0530

    solved majorityElement()-169 question

---
+++ lib/majority_element_169.dart
@@ -0,0 +1,19 @@
+import 'dart:collection';
+
+int majorityElement(List<int> nums) {
+  HashMap<int, int> h = HashMap();
+  for (int i = 0; i < nums.length; i++) {
+    h[nums[i]] = (h[nums[i]] ?? 0) + 1;
+  }
+
+  int max = 0;
+  int value = -1;
+  for (int i in h.keys) {
+    if (h[i]! > max) {
+      max = h[i]!;
+      value = i;
+    }
+  }
+
+  return value;
+}
